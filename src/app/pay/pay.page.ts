9ada1aea1759250d5aa424028da2b392297c6ff0
“Chris <chris.fan.dev@gmail.com>
2018-11-03 15:58:49 +0800

    fix qrcode not display on android, Closes #6

--- src/app/pay/pay.page.ts
+++ src/app/pay/pay.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import QRious from 'qrious';
+
 import { PayService } from './pay.service';
 
 @Component({
@@ -8,12 +10,36 @@ import { PayService } from './pay.service';
   styleUrls: ['pay.page.scss'],
 })
 export class PayPage implements OnInit {
+  qrcode = '';
   constructor(
     private router: Router,
     private payService: PayService,
   ) {}
 
   ngOnInit() {
+    if (this.payService.orderInfo.code_url) {
+      this.drawQrCode();
+    } else {
+      // TODO: 需要监听 code_url 变化
+      setTimeout(() => {
+        this.drawQrCode();
+      }, 1000);
+    }
+  }
+
+  drawQrCode() {
+    const qr = new QRious({
+      // element: document.getElementById('qr'),
+      size: 300,
+      value: this.payService.orderInfo.code_url,
+      background: 'white',
+      foreground: 'black',
+      backgroundAlpha: 1,
+      foregroundAlpha: 1,
+      mime: 'image/png',
+    });
+
+    this.qrcode = qr.toDataURL();
   }
 
   goBack() {
